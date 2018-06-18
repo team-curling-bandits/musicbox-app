ea9ca24d7059260485a728e80eabbe174f050e62
Chris Golden <chrgolden@gmail.com>
2018-06-18 16:52:51 -0700

    song player ready, with mock data

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,16 @@
 <template>
-  
+<div>
+  <h2>Log In</h2>
+  <form>
+    <input type="text" placeholder="Name">
+  </form>
+</div>
 </template>
 
 <script>
 export default {
 
-}
+};
 </script>
 
 <style>
--- src/components/Public.vue
+++ src/components/Public.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Public page</h1>
+  <div>
+    <h1>Public page</h1>
+    <div class="music-player">
+      <h2>Song: Say It Ain't So</h2>
+      <h3>Artist: Weezer</h3>
+      <h3>Album: Weezer (Blue Album)(Deluxe Edition)</h3>
+      <audio controls>
+        <!--example song, soon to be dynamic-->
+        <source src="http://listen.vo.llnwd.net/g2/4/2/4/9/8/911189424.mp3">
+      </audio>
+    </div>
+  </div>
 </template>
 
 <script>
-import { getMusic } from '../service/api';
+import { getMusic } from '../services/api';
 
 export default {
   data() {
@@ -22,5 +33,10 @@ export default {
 </script>
 
 <style>
-
+.music-player {
+  margin: auto;
+  width: 300px;
+  height: 300px;
+  
+}
 </style>

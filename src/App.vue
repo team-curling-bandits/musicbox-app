dcc86fc9eed946affb21ea0768cb66b616f676e5
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-18 16:08:19 -0700

    Added nav links updated router.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <h1>Music Box</h1>
+    <nav>
+    <router-link to="/" class="nav">Home</router-link> 
+    <router-link to="/about" class="nav">About Us</router-link>
+    <router-link to="/public" class="nav">Public Page</router-link>
+    <router-link to="/user" class="nav">User Page</router-link>
+    <router-link to="/news" class="nav">News</router-link>
+    <router-link to="/login" class="nav">Login / SignUp</router-link>
+    </nav>
+    <hr>
+
+    <router-view></router-view>
 
   </div>
 </template>
@@ -8,10 +20,22 @@
 
 
 export default {
-
+  
 };
 </script>
 
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  border: 2px solid black;
+}
+.nav {
+  margin: 10px;
+}
 
 </style>
--- src/router.js
+++ src/router.js
@@ -1,27 +1,19 @@
 import VueRouter from 'vue-router';
-import Home from './components/Home.vue';
-import About from './components/About.vue';
-import Quadrants from './components/Quadrants.vue';
-import QuadrantDetail from './components/QuadrantDetail.vue';
-import NeighborhoodsList from './components/NeighborhoodsList.vue';
-import NeighborhoodsMap from './components/NeighborhoodsMap.vue';
-import NewNeighborhood from './components/NewNeighborhood.vue';
+import Home from './components/Home';
+import About from './components/About';
+import Public from './components/Public';
+import User from './components/User';
+import News from './components/News';
+import Login from './components/Login';
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/about', component: About },
-    { path: '/whatever', component: Quadrants },
-    { 
-      path: '/quadrants/:id', 
-      component: QuadrantDetail,
-      children: [
-        { path: 'list', component: NeighborhoodsList },
-        { path: 'map', component: NeighborhoodsMap },
-        { path: 'new', component: NewNeighborhood },
-        { path: '', redirect: 'list' }
-      ]
-    },
+    { path: '/public', component: Public },
+    { path: '/user', component: User },
+    { path: '/news', component: News },
+    { path: '/login', component: Login },
     { path: '*', redirect: '/' }
   ]
 });

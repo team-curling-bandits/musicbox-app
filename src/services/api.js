a944f780094caa4f0e0ee20e908b59c1235d1c93
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-19 14:47:13 -0700

    added fetch requests for users and savedsongs in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,18 @@
-// const URL = 'http://localhost:3000/api';
+const URL = 'http://localhost:3000/api';
 const searchUrl = 'https://api.napster.com/v2.2/search?query=';
-const MusicBox_URL = `${URL}/musicbox`;
+const MUSICBOX_URL = `${URL}/musicbox`;
+const USERS_URL = `${URL}/users`;
+const SAVEDSONGS_URL = `${URL}/savedsongs`;
+
+function responseHandler(response) {
+  if(response.ok) return response.json();
+  return response.json().then(err => {
+    throw err.message;
+  });
+}
 
 export function getMusic() {
-  return fetch(MusicBox_URL, {
+  return fetch(MUSICBOX_URL, {
     headers: { 'Content-Type': 'application/json' }
   });
 }
@@ -14,4 +23,32 @@ export function searchSong(song) {
   const url = searchUrl + encodeURIComponent(song) + '&type=track&apikey=' + 'ZWQwYzkzOTctNzY5Ni00ZjkwLTkyODMtMDc3YTQwMDFmN2Y5';
   return fetch(url)
     .then(response => response.json());
+}
+
+export function getUsers() {
+  return fetch(USERS_URL, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(responseHandler);
+}
+
+export function getUser(id) {
+  return fetch(`${URL}/users/${id}`, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(responseHandler);
+}
+
+export function getSavedSongs() {
+  return fetch(SAVEDSONGS_URL, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(responseHandler);
+}
+
+export function getSavedSong(id) {
+  return fetch(`${URL}/savedsongs/${id}`, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(responseHandler);
 }

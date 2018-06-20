4eeac5756af4901bf611dbe2ebc0241a792a7eb4
Chris Golden <chrgolden@gmail.com>
2018-06-19 22:39:34 -0700

    log in and sign up functioning

--- src/services/api.js
+++ src/services/api.js
@@ -3,13 +3,14 @@ const searchUrl = 'https://api.napster.com/v2.2/search?query=';
 const MUSICBOX_URL = `${URL}/musicbox`;
 const USERS_URL = `${URL}/users`;
 const SAVEDSONGS_URL = `${URL}/savedsongs`;
+const AUTH_URL = `${URL}/auth`;
 
-// function responseHandler(response) {
-//   if(response.ok) return response.json();
-//   return response.json().then(err => {
-//     throw err.message;
-//   });
-// }
+function responseHandler(response) {
+  if(response.ok) return response.json();
+  return response.json().then(err => {
+    throw err.message;
+  });
+}
 
 export function getMusic() {
   return fetch(MUSICBOX_URL, {
@@ -22,34 +23,69 @@ export function searchSong(song) {
   const apiKey = process.env.API_KEY;
   const url = searchUrl + encodeURIComponent(song) + '&type=track&apikey=' + 'ZWQwYzkzOTctNzY5Ni00ZjkwLTkyODMtMDc3YTQwMDFmN2Y5';
   return fetch(url)
-    .then(response => response.json());
+    .then(responseHandler);
 }
 
 export function getUsers() {
   return fetch(USERS_URL, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(response => response.json());
+    .then(responseHandler);
 }
 
 export function getUser() {
   return fetch(`${URL}/users/1`, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(response => response.json());
+    .then(responseHandler);
 }
 
 export function getSavedSongs() {
   return fetch(SAVEDSONGS_URL, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(response => response.json());
+    .then(responseHandler);
 }
 
 export function getSavedSong(id) {
   return fetch(`${URL}/savedsongs/${id}`, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(response => response.json());
+    .then(responseHandler);
 }
 
+function getHeaders(hasBody) {
+  const headers = {};
+  if(hasBody) {
+    headers['Content-Type'] = 'application/json';
+  }
+
+  const user = localStorage.user;
+  if(user) {
+    try {
+      headers['Authorization'] = JSON.parse(user).id;
+    }
+    catch (err) {
+      localStorage.removeItem('user');
+    }
+  }
+
+  return headers;
+}
+export function signUp(credentials) {
+  return fetch(`${AUTH_URL}/signup`, {
+    method: 'POST',
+    headers: getHeaders(true),
+    body: JSON.stringify(credentials)
+  })
+    .then(responseHandler);
+}
+
+export function signIn(credentials) {
+  return fetch(`${AUTH_URL}/signin`, {
+    method: 'POST',
+    headers: getHeaders(true),
+    body: JSON.stringify(credentials)
+  })
+    .then(responseHandler);
+}

c0bbc35356aa0ab32b19eebad7e0d0a6f1540bda
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-19 17:21:28 -0700

    Finishing user wire connection

--- src/services/api.js
+++ src/services/api.js
@@ -4,12 +4,12 @@ const MUSICBOX_URL = `${URL}/musicbox`;
 const USERS_URL = `${URL}/users`;
 const SAVEDSONGS_URL = `${URL}/savedsongs`;
 
-function responseHandler(response) {
-  if(response.ok) return response.json();
-  return response.json().then(err => {
-    throw err.message;
-  });
-}
+// function responseHandler(response) {
+//   if(response.ok) return response.json();
+//   return response.json().then(err => {
+//     throw err.message;
+//   });
+// }
 
 export function getMusic() {
   return fetch(MUSICBOX_URL, {
@@ -29,26 +29,26 @@ export function getUsers() {
   return fetch(USERS_URL, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(responseHandler);
+    .then(response => response.json());
 }
 
-export function getUser(id) {
-  return fetch(`${URL}/users/${id}`, {
+export function getUser() {
+  return fetch(`${URL}/users/1`, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(responseHandler);
+    .then(response => response.json());
 }
 
 export function getSavedSongs() {
   return fetch(SAVEDSONGS_URL, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(responseHandler);
+    .then(response => response.json());
 }
 
 export function getSavedSong(id) {
   return fetch(`${URL}/savedsongs/${id}`, {
     headers: { 'Content-Type': 'application/json' }
   })
-    .then(responseHandler);
+    .then(response => response.json());
 }

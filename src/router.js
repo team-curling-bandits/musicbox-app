197e6249d2959c90dd1d520df97ebf56e358f408
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-21 16:12:55 -0700

    Clean up code

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import About from './components/About';
 import Browse from './components/Browse';
 import User from './components/User';
 import Community from './components/Community';
-// import CommunityDetail from './components/CommunityDetail';
 import CommunityPage from './components/CommunityPage';
 import News from './components/News';
 import Login from './components/Login';
